76a2a3965e7abdf99a53956b20162fb13f44c8d9
aluyolo04 <160716285+aluyolo04@users.noreply.github.com>
2026-05-30 18:29:22 +0200

    Added part 2 Assignmnet features

---
+++ CyberlinkGUI/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CyberlinkGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            rtbChat = new RichTextBox();
+            txtMessage = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(304, 30);
+            label1.TabIndex = 0;
+            label1.Text = "CYBERLINK Cybersecurity Bot";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // rtbChat
+            // 
+            rtbChat.BackColor = SystemColors.ControlLight;
+            rtbChat.Location = new Point(12, 42);
+            rtbChat.Name = "rtbChat";
+            rtbChat.ReadOnly = true;
+            rtbChat.Size = new Size(776, 367);
+            rtbChat.TabIndex = 1;
+            rtbChat.Text = "";
+            rtbChat.TextChanged += richTextBox1_TextChanged;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(12, 415);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(695, 23);
+            txtMessage.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = Color.Coral;
+            btnSend.Location = new Point(713, 415);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(75, 23);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "Send";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSend);
+            Controls.Add(txtMessage);
+            Controls.Add(rtbChat);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private RichTextBox rtbChat;
+        private TextBox txtMessage;
+        private Button btnSend;
+    }
+}
